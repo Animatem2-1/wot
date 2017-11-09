cb91c4c2922cc95d8515d93fd4476ea27e4c5ad7
= <=>
2017-11-09 15:28:24 +0100

    wtyczki

---
+++ plugins/internal/DHT22SensorPlugin.js
@@ -0,0 +1,64 @@
+var resources = require('./../../resources/model');
+  //utils = require('./../../utils/utils.js');
+var interval, sensor;
+var model = resources.pi.sensors;
+var pluginName = 'Temperatura i Wilgotność';
+var localParams = {'simulate' : false, 'frequency' : 5000};
+
+exports.start = function (params) {
+  localParams = params;
+  if (params.simulate) {
+    simulate();
+  } else {
+    connectHardware();
+  }
+};
+
+exports.stop = function () {
+  if (params.simulate) {
+    clearInterval(interval);
+  } else {
+    sensor.unexport();
+  }
+  console.info('Wtyczka %s została zatrzymana!', pluginName);
+};
+
+function connectHardware() {
+  var sensorDriver = require('node-dht-sensor');
+  var sensor = {
+    initialize: function () {
+      return sensorDriver.initialize(22, model.temperature.gpio);
+    },
+    read: function () {
+      var readout = sensorDriver.read();
+      model.temperature.value = parseFloat(readout.temperature.toFixed(2));
+      model.humidity.value = parseFloat(readout.humidity.toFixed(2));
+      showValue();
+
+      setTimeout(function () {
+        sensor.read();
+      }, localParams.frequency);
+    }
+  };
+  if (sensor.initialize()) {
+    console.info('Hardware %s sensor started!', pluginName);
+    sensor.read();
+  } else {
+    console.warn('Failed tto Initialize sensor!');
+  }
+};
+
+
+function simulate() {
+  setInterval(function () {
+    model.temperature.value = utils.randomInt(0, 40);
+    model.humidity.value = utils.randomInt(0, 100);
+    showValue();
+  }, localParams.frequency);
+  console.info('Simulated %s sensor started!', pluginName);
+};
+
+function showValue() {
+  console.info('Temperatura: %s C, Wilgotność: %s \%',
+    model.temperature.value, model.humidity.value);
+};
--- wot-server.js
+++ wot-server.js
@@ -1,6 +1,12 @@
 var httpServer = require('./servers/http'),
   resources = require('./../resources/model');
 
+var ledPlugin = require('./plugins/internal/ledsPlugin'),
+  dhtPlugin = require('./plugins/internal/DHT22SensorPlugin.js');
+
+ledsPlugin.start({'simulate': true, 'frequency': 2000});
+dhtPlugin.start({'simulate': true, 'frequency': 5000})
+
 var server = httpServer.listen(resources.pi.port, function () {
   console.info('Moc jest z nami i działa na porcie %s', resources.pi.port);
 });
