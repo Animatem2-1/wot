85474a43faf45ca52f1ef12d78255cdedfaf3383
= <=>
2017-11-09 16:21:42 +0100

    poprawki do wtyczek

--- wot-server.js
+++ wot-server.js
@@ -4,8 +4,8 @@ var httpServer = require('./servers/http'),
 var ledPlugin = require('./plugins/internal/ledsPlugin'),
   dhtPlugin = require('./plugins/internal/DHT22SensorPlugin.js');
 
-ledsPlugin.start({'simulate': true, 'frequency': 2000});
-dhtPlugin.start({'simulate': true, 'frequency': 5000})
+ledsPlugin.start({'simulate': false, 'frequency': 2000});
+dhtPlugin.start({'simulate': false, 'frequency': 5000})
 
 var server = httpServer.listen(resources.pi.port, function () {
   console.info('Moc jest z nami i działa na porcie %s', resources.pi.port);
